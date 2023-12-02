a125725212b1db5cb1068391ad1df23da3d14d98
EvoNick-IR <mostafa.haghani69@Gmail.com>
2023-12-02 13:19:17 +0330

    build: slider assets added

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const slider = () => {
-  return <div className="w-[100vw] relative h-[500px] bg-orange-600"></div>;
+  return (
+    <div className=" flex items-center justify-center w-[100vw] relative h-[500px] bg-orange-600">
+      <div id="gallery" className="w-[100vw] h-[100%] bg-slate-500">
+        <div id="pics" className="w-[500%]">
+            <img src="" alt="" />
+        </div>
+      </div>
+      <div id="sliderMidContainer"> </div>
+    </div>
+  );
 };
 
 export default slider;
