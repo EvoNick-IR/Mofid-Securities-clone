841be86632a9421fdc6dfc8b1bc83ae2c0af9058
EvoNick-IR <mostafa.haghani69@Gmail.com>
2023-12-05 17:08:43 +0330

    feat: Amoozesh component created days component finished

--- src/components/Amoozesh/Classes.jsx
+++ src/components/Amoozesh/Classes.jsx
@@ -1,9 +1,10 @@
 import Button_noblue from "../Buttons/Button_noblue";
+import Days from "./Days";
 
 const Classes = () => {
   return (
     <div className="bg-[#F8F9FA]  w-[1140px] flex flex-nowrap justify-between rounded-md p-8 text-slate-800">
-      <div className="flex flex-col items-start w-3/12 gap-10 ">
+      <div className="flex flex-col items-start w-4/12 pl-6 gap-10 ">
         <div className="flex flex-col gap-10">
           <h2 className="isbold text-2xl">کلاس‌های آموزشی روزانه رایگان</h2>
           <p className=" text-slate-700">
@@ -16,8 +17,14 @@ const Classes = () => {
           <Button_noblue title="آرشیو رویدادهای آموزشی" />
         </div>
       </div>
-      <div className="flex items-start h-9 w-2/12 bg-amber-700"></div>
-      <div className="flex items-start h-9 w-6/12 bg-green-600"></div>
+      <div className="flex flex-col items-center  w-2/12 gap-10 pr-6">
+        <Days day="شنبه" tarikh="11 آذر" />
+        <Days day="یکشنبه" tarikh="12 آذر" />
+        <Days day="دوشنبه" tarikh="13 آذر" />
+        <Days day="سشنبه" tarikh="14 آذر" />
+        <Days day="چهارشنبه" tarikh="15 آذر" />
+      </div>
+      <div className="flex items-start  w-6/12"></div>
     </div>
   );
 };
--- src/components/Amoozesh/Days.jsx
+++ src/components/Amoozesh/Days.jsx
@@ -1,5 +1,14 @@
-const Days = () => {
-  return <div>Days</div>;
+const Days = (props) => {
+  return (
+    <div className="flex w-2/3 flex-col gap-1 p-4 border-r-2">
+      <div className="flex justify-center text-xl">
+        <span className="">{props.day}</span>
+      </div>
+      <div className="flex justify-start ">
+        <span>{props.tarikh}</span>
+      </div>
+    </div>
+  );
 };
 
 export default Days;
