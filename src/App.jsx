240b3d1f76f44bf2e14c4a6f279cbff30b580172
EvoNick-IR <mostafa.haghani69@Gmail.com>
2023-12-07 17:06:41 +0330

    fix: chatIcon folder renamed to ChatIcon

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Header from "./components/Header/Header";
-import ChatIcon from "./components/chatIcon/ChatIcon";
+import ChatIcon from "./components/ChatIcon/ChatIcon";
 
 export default function App() {
   return (
