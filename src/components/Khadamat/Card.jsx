9c29e246a561ae2dec964a820eb86272238a8e90
EvoNick-IR <mostafa.haghani69@Gmail.com>
2023-12-14 02:53:57 +0330

    fix: responsive cards fixed

--- src/components/Khadamat/Card.jsx
+++ src/components/Khadamat/Card.jsx
@@ -1,12 +1,12 @@
 const Card = (props) => {
   const item = props.data;
   return (
-    <div className="flex flex-col gap-3 items-center justify-start bg-[#052840] h-[170px] p-2 w-36 rounded-md">
-      <div className=" flex justify-center rounded-full w-20 h-20 bg-[#00334D]">
+    <div className="flex flex-col gap-3 items-center text-sm md:text-base justify-start bg-[#052840] h-44 p-2 w-36 rounded-md ">
+      <div className=" flex justify-center rounded-full w-20 h-20  bg-[#00334D]">
         <img src={item.pic} alt={item.title} className="w-8" />
       </div>
       <div>
-        <span className="text-sm ismed">{item.title}</span>
+        <span className=" ismed">{item.title}</span>
       </div>
     </div>
   );
