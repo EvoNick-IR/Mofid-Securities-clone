4a5e2d55b5665c29e577023f3946af47f354e74d
EvoNick-IR <mostafa.haghani69@Gmail.com>
2023-12-24 18:29:42 +0330

    fix: fixed EasyTrade Component responsive

--- src/components/EasyTrade/EasyTrade.jsx
+++ src/components/EasyTrade/EasyTrade.jsx
@@ -8,8 +8,8 @@ import {
 
 const EasyTrade = () => {
   return (
-    <div className="max-w-[1140px] w-full px-4 flex flex-col justify-center items-center mt-[128px]  text-white gap-12">
-      <div className="flex gap-4">
+    <div className="max-w-[1140px] w-full px-4 flex flex-col justify-center items-center mt-[128px] text-white">
+      <div className="flex gap-4 mb-10">
         <div className="flex flex-col ismed gap-2 text-xs">
           <img src={rainbow_larg} alt="" className="w-[63px] h-[42px]" />
           <div className="flex ">
@@ -22,24 +22,25 @@ const EasyTrade = () => {
           <h3 className="isreg text-base">ساده اما حرفه ای</h3>
         </div>
       </div>
-      <img src={easyTrade} alt="" className="w-full block" />
-      <div className="flex flex-wrap w-full items-end gap-16 text-base isreg text-slate-300">
-        <div className="flex px-4 ">
-          <p className="">
+
+      <img src={easyTrade} alt="" className="block w-full" />
+
+      <div className="isreg text-slate-300 mt-2 flex flex-col items-center gap-2 lg:gap-6 md:flex-row md:justify-between md:w-[95%] ">
+        <div className="md:w-4/5 lg:w-2/3">
+          <p className="text-center text-sm md:text-right lg:text-lg">
             ایزی‌تریدر هم اکنون بیشترین تعداد کاربر را در میان سامانه‌های
             معاملاتی در بازار سرمایه کشور داراست. این نرم‌افزار با محیطی ساده،
             امکاناتی حرفه‌ای را در اختیار معامله‌گران قرار می‌دهد. ایزی‌تریدر در
             دو نسخه وب و موبایل قابل استفاده است.
           </p>
         </div>
-
-        <div className="w-full">
+        <div className="w-full md:w-1/5">
           <Button_nogreen title="اظلاعات بیشتر" />
         </div>
       </div>
-      <div className="flex flex-wrap  items-end gap-10 text-base isreg text-slate-100">
-        <div className="w-full flex px-4 py-8 rounded-md gap-6 flex-col  bg-[#052840]">
-          <div className="flex gap-2 w-full ">
+      <div className="flex flex-wrap mt-16  items-end gap-10 text-base isreg text-slate-100 mb-16 justify-center lg:flex-nowrap lg:w-[85%] ">
+        <div className="flex px-4 py-8 rounded-md gap-6 flex-col  bg-[#052840] md:w-1/2 lg:h-[270px]">
+          <div className="flex gap-2  ">
             <img src={hami} alt="" />
             <div className="flex flex-col gap-2">
               <h2>طرح حامی</h2>
@@ -57,7 +58,7 @@ const EasyTrade = () => {
             <Button_nogreen title="اطلاعات بیشتر" />
           </div>
         </div>
-        <div className="w-full flex px-4 py-8 rounded-md gap-6 flex-col  bg-[#052840]">
+        <div className="flex px-4 py-8 rounded-md gap-6 flex-col  bg-[#052840] md:w-1/2 lg:h-[270px]">
           <div className="flex gap-2 w-full ">
             <img src={karmozd} alt="" className="" />
             <div className="flex flex-col gap-2">
