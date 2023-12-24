427fcc771f6158f936aeafd110bb5b9ea125063c
EvoNick-IR <mostafa.haghani69@Gmail.com>
2023-12-24 18:30:49 +0330

    fix: change Bottoms whiteSpace to noWrap

--- src/components/Buttons/Button_nogreen.jsx
+++ src/components/Buttons/Button_nogreen.jsx
@@ -9,7 +9,7 @@ const Button_bg = (props) => {
     setBtnTitle(title);
   }, [title]);
   return (
-    <div className="text-[#00A796] gap-3  flex justify-center p-3 items-center ismed text-sm border-solid border-[1px]  border-[#00A796]  transition-all duration-300 cursor-pointer rounded-md hover:bg-gray-700">
+    <div className="text-[#00A796] gap-3  flex justify-center p-3 items-center whitespace-nowrap ismed text-sm border-solid border-[1px]  border-[#00A796]  transition-all duration-300 cursor-pointer rounded-md hover:bg-gray-700">
       <button type="button">{btnTitle}</button>
       <img src={arrowLeft} alt="" className="w-3  " />
     </div>
